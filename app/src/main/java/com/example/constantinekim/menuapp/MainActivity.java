051d3542559751aee2e42ba5d46dfd8c027f9558
Constantine Kim <electro.passion92@gmail.com>
2020-07-26 20:26:23 +0900

    Project Created

---
+++ app/src/main/java/com/example/constantinekim/menuapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.constantinekim.menuapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        menu.add(0, 0, 0, "Settings");
+//        menu.add(0, 1, 1, "Open Settings");
+//        menu.add(0, 2, 2, "Save Settings");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        TextView textView = findViewById(R.id.header_text);
+
+        switch (id) {
+            case R.id.action_settings:
+                textView.setText("Settings");
+                return true;
+            case R.id.open_settings:
+                textView.setText("Open settings");
+                return true;
+            case R.id.save_settings:
+                textView.setText("Save Settings");
+                return true;
+            case R.id.feedback:
+                textView.setText("Feedback");
+                return true;
+            case R.id.forward:
+                textView.setText("Forward");
+                return true;
+            case R.id.back:
+                textView.setText("Back");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
